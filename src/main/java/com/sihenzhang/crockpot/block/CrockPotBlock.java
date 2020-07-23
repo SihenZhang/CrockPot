eeb73a43a8f67ee9176bca1ba74f0332df1b1d89
SeraphJACK <seraphjack@outlook.com>
2020-07-23 12:54:04 +0800

    Add facing state

--- src/main/java/com/sihenzhang/crockpot/block/CrockPotBlock.java
+++ src/main/java/com/sihenzhang/crockpot/block/CrockPotBlock.java
@@ -1,15 +1,21 @@
 package com.sihenzhang.crockpot.block;
 
 import com.sihenzhang.crockpot.tile.CrockPotTileEntity;
+import mcp.MethodsReturnNonnullByDefault;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalBlock;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.ItemStack;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.StateContainer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
+import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.IBlockReader;
@@ -18,8 +24,14 @@ import net.minecraftforge.fml.network.NetworkHooks;
 import net.minecraftforge.items.CapabilityItemHandler;
 
 import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
 
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
 public abstract class CrockPotBlock extends Block {
+
+    public static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
+
     public CrockPotBlock() {
         super(Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F).notSolid());
     }
@@ -36,6 +48,7 @@ public abstract class CrockPotBlock extends Block {
     }
 
     @Override
+    @SuppressWarnings("deprecation")
     public void onReplaced(BlockState state, World worldIn, BlockPos pos, BlockState newState, boolean isMoving) {
         TileEntity tileEntity = worldIn.getTileEntity(pos);
         if (tileEntity != null && state.getBlock() != newState.getBlock()) {
@@ -53,14 +66,33 @@ public abstract class CrockPotBlock extends Block {
     }
 
     @Override
+    @SuppressWarnings("deprecation")
     public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
         if (!worldIn.isRemote && handIn == Hand.MAIN_HAND) {
             CrockPotTileEntity tileEntity = (CrockPotTileEntity) worldIn.getTileEntity(pos);
             NetworkHooks.openGui((ServerPlayerEntity) player, tileEntity, (packetBuffer -> {
+                assert tileEntity != null;
                 packetBuffer.writeBlockPos(tileEntity.getPos());
             }));
         }
         return ActionResultType.SUCCESS;
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        return this.getDefaultState().with(FACING, context.getPlacementHorizontalFacing().getOpposite());
+    }
+
+    @Override
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public BlockState rotate(BlockState state, Rotation rot) {
+        return state.with(FACING, rot.rotate(state.get(FACING)));
     }
 
     public abstract int getPotLevel();
