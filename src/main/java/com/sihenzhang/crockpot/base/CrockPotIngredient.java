525cd9f1fdd109978f44345161fc326d7cf90e80
SihenZhang <1334125013@qq.com>
2020-08-01 10:33:45 +0800

    使用Minecraft自带的JSONUtils进行替换

--- src/main/java/com/sihenzhang/crockpot/base/CrockPotIngredient.java
+++ src/main/java/com/sihenzhang/crockpot/base/CrockPotIngredient.java
@@ -1,11 +1,13 @@
 package com.sihenzhang.crockpot.base;
 
 import com.google.gson.*;
-import com.sihenzhang.crockpot.util.JsonUtils;
 import net.minecraft.item.Item;
+import net.minecraft.util.JSONUtils;
 
 import java.lang.reflect.Type;
 import java.util.EnumMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class CrockPotIngredient {
     protected Item item;
@@ -31,18 +33,26 @@ public class CrockPotIngredient {
     public static class Serializer implements JsonDeserializer<CrockPotIngredient>, JsonSerializer<CrockPotIngredient> {
         @Override
         public CrockPotIngredient deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-            JsonObject obj = json.getAsJsonObject();
-            Item item = JsonUtils.deserializeItem(obj.get("ingredient").getAsString());
-            EnumMap<CrockPotIngredientType, Float> ingredientValue = JsonUtils.deserializeIngredientValue(obj.get("value").getAsJsonObject());
+            JsonObject object = JSONUtils.getJsonObject(json, "crock pot ingredient");
+            Item item = JSONUtils.getItem(object, "ingredient");
+            JsonObject ingredientValueJsonObject = JSONUtils.getJsonObject(object, "value");
+            EnumMap<CrockPotIngredientType, Float> ingredientValue = new EnumMap<>(CrockPotIngredientType.class);
+            for (Map.Entry<String, JsonElement> entry : ingredientValueJsonObject.entrySet()) {
+                ingredientValue.put(CrockPotIngredientType.valueOf(entry.getKey().toUpperCase()), JSONUtils.getFloat(entry.getValue(), "crock pot ingredient value"));
+            }
             return new CrockPotIngredient(item, ingredientValue);
         }
 
         @Override
         public JsonElement serialize(CrockPotIngredient src, Type typeOfSrc, JsonSerializationContext context) {
-            JsonObject obj = new JsonObject();
-            obj.addProperty("ingredient", JsonUtils.serializeItem(src.item));
-            obj.add("value", JsonUtils.serializeIngredientValue(src.ingredientValue));
-            return obj;
+            JsonObject object = new JsonObject();
+            object.addProperty("ingredient", Objects.requireNonNull(src.item.getRegistryName()).toString());
+            JsonObject ingredientValueJsonObject = new JsonObject();
+            for (Map.Entry<CrockPotIngredientType, Float> entry : src.ingredientValue.entrySet()) {
+                ingredientValueJsonObject.addProperty(entry.getKey().name(), entry.getValue());
+            }
+            object.add("value", ingredientValueJsonObject);
+            return object;
         }
     }
 }
