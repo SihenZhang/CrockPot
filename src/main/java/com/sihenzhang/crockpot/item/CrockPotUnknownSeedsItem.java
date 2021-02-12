8cf81ad3a55e86d12d6cdf33c9da7b5491896a1e
SihenZhang <1334125013@qq.com>
2021-02-12 20:23:56 +0800

    Change key name in config, Unknown Seeds can be fed to Chicken andd Parrot, fix desc in book

--- src/main/java/com/sihenzhang/crockpot/item/CrockPotUnknownSeedsItem.java
+++ src/main/java/com/sihenzhang/crockpot/item/CrockPotUnknownSeedsItem.java
@@ -1,16 +1,14 @@
 package com.sihenzhang.crockpot.item;
 
-import com.sihenzhang.crockpot.CrockPot;
 import com.sihenzhang.crockpot.CrockPotConfig;
 import com.sihenzhang.crockpot.CrockPotRegistry;
-import net.minecraft.item.BlockNamedItem;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
 
-public class CrockPotUnknownSeedsItem extends BlockNamedItem {
+public class CrockPotUnknownSeedsItem extends CrockPotSeedsItem {
     public CrockPotUnknownSeedsItem() {
-        super(CrockPotRegistry.unknownCrops.get(), new Properties().group(CrockPot.ITEM_GROUP));
+        super(CrockPotRegistry.unknownCropsBlock.get());
     }
 
     @Override
