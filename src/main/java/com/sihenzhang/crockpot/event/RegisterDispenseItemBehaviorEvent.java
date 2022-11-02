16fd5f2fc57e224cadf29e87d8ffbf26958ee7cd
SihenZhang <1334125013@qq.com>
2022-11-02 17:59:13 +0800

    Register Dispenser Behavior for Parrot Egg

---
+++ src/main/java/com/sihenzhang/crockpot/event/RegisterDispenseItemBehaviorEvent.java
@@ -0,0 +1,28 @@
+package com.sihenzhang.crockpot.event;
+
+import com.sihenzhang.crockpot.CrockPot;
+import com.sihenzhang.crockpot.CrockPotRegistry;
+import com.sihenzhang.crockpot.entity.ThrownParrotEgg;
+import net.minecraft.Util;
+import net.minecraft.core.Position;
+import net.minecraft.core.dispenser.AbstractProjectileDispenseBehavior;
+import net.minecraft.world.entity.projectile.Projectile;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.DispenserBlock;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+
+@Mod.EventBusSubscriber(modid = CrockPot.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class RegisterDispenseItemBehaviorEvent {
+    @SubscribeEvent
+    public static void onLoadComplete(FMLLoadCompleteEvent event) {
+        event.enqueueWork(() -> CrockPotRegistry.PARROT_EGGS.forEach((variant, egg) -> DispenserBlock.registerBehavior(egg.get(), new AbstractProjectileDispenseBehavior() {
+            @Override
+            protected Projectile getProjectile(Level pLevel, Position pPosition, ItemStack pStack) {
+                return Util.make(new ThrownParrotEgg(pLevel, pPosition.x(), pPosition.y(), pPosition.z()), entity -> entity.setItem(pStack));
+            }
+        })));
+    }
+}
