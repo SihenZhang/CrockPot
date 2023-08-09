83c3a6488b3a136195b71b8bdc0f25ee559c0ae9
SihenZhang <1334125013@qq.com>
2023-08-10 00:49:26 +0800

    Crock Pot will drop container items

--- src/main/java/com/sihenzhang/crockpot/block/entity/CrockPotBlockEntity.java
+++ src/main/java/com/sihenzhang/crockpot/block/entity/CrockPotBlockEntity.java
@@ -14,6 +14,7 @@ import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
 import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.Containers;
 import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
@@ -103,6 +104,7 @@ public class CrockPotBlockEntity extends BlockEntity implements MenuProvider {
                     blockEntity.cookingTotalTime = blockEntity.getActualCookingTotalTime(recipe);
                     blockEntity.result = recipe.assemble(recipeWrapper, level.registryAccess());
                     blockEntity.shrinkInputs();
+                    Containers.dropContents(level, pos, recipe.getRemainingItems(recipeWrapper));
                     blockEntity.hasChanged = true;
                 });
             }
